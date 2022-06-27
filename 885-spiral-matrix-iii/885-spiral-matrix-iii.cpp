f3154c85b2e238f7403507a5664d7a517cf00a18
Soumya Singh <somu12.ss@gmail.com>
2022-06-27 17:23:02 +0530

    Time: 16 ms (88.49%), Space: 12.1 MB (89.80%) - LeetHub

--- 885-spiral-matrix-iii/885-spiral-matrix-iii.cpp
+++ 885-spiral-matrix-iii/885-spiral-matrix-iii.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
         vector<vector<int>> res = {{rStart, cStart}};
-        int dx = 0, dy = 1, tmp;
+        int rowMov = 0, colMov = 1, tmp;
         for (int n = 0; res.size() < rows * cols; n++) {
             for (int i = 0; i < n / 2 + 1; i++) {
-                rStart += dx, cStart += dy;
+                rStart += rowMov, cStart += colMov;
                 if (0 <= rStart && rStart < rows && 0 <= cStart && cStart < cols)
                     res.push_back({rStart, cStart});
             }
-            tmp = dx, dx = dy, dy = -tmp;
+            tmp = rowMov, rowMov = colMov, colMov = -tmp;
         }
         return res;
     }

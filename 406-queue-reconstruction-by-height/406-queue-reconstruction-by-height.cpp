c3ff014d23ce9aacd211f55dad2d82154f58d47d
Soumya Singh <somu12.ss@gmail.com>
2022-06-30 04:59:25 +0530

    Time: 270 ms (26.42%), Space: 25.3 MB (5.81%) - LeetHub

--- 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/406-queue-reconstruction-by-height.cpp
@@ -3,35 +3,13 @@ public:
     
     // bool compare()
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        vector<vector<int>> ans(people.size(), vector<int>(2,-1));
-        sort(people.begin(), people.end());
-        // for(auto i : people){
-        //     cout<<i[0]<<" "<<i[1]<<endl;
-        // }
-        int last=0;
-        for(int i=0; i<people.size(); i++){
-            int j = 0,count=0;
-                while(count<people[i][1]){
-                    if(ans[j][1]==-1 || ans[j][0]>=people[i][0])
-                        count++;
-                    j++;
-                }
-                while(ans[j][1]!=-1)
-                    j++;
-                ans[j]=people[i];
-            // int j = people[i][1];
-            // if(last==0){
-            //     ans[j]=people[i];
-            // }
-            // else{
-            //     while(ans[j][1]!=-1){
-            //         last++;             
-            //     }
-            //     int k
-            // }
-
-
+        sort(people.begin(), people.end(),[](vector<int> p1, vector<int> p2){
+            return p1[0] > p2[0] || (p1[0] == p2[0] && p1[1] < p2[1]);
+        });
+        vector<vector<int>> sol;
+        for (auto person : people){
+            sol.insert(sol.begin() + person[1], person);
         }
-        return ans;
+        return sol;
     }
 };

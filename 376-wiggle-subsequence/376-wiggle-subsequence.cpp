32454ed5ffe2a8b9f0fb2df90b3a15c39644bd98
Soumya Singh <somu12.ss@gmail.com>
2022-07-05 16:34:23 +0530

    Time: 2 ms (68.68%), Space: 7.1 MB (71.64%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,85 +1,56 @@
 class Solution {
 public:
     
-
+    int dp[1005][2];
     
-//     int helper(vector<int>& nums, int i, int n, bool flag)
-//     {
+    int helper(vector<int>& nums, int i, int n, bool flag)
+    {
         
-//         if(i == n - 1) //sirf ek element
-//             return 1;
+        if(i == n - 1) //sirf ek element
+            return 1;
 
         
-//         if(dp[i][flag] != -1)
-//             return dp[i][flag];
+        if(dp[i][flag] != -1)
+            return dp[i][flag];
+        int Take = 0;
+        int notTake = 0;
         
-//         int ans = 0;
+        if(flag) 
+        {
+            
+            if(nums[i + 1] > nums[i])  // include
+            {
+                 Take =  1 + helper(nums, i + 1, n, false);
+            }
+             notTake = helper(nums, i + 1, n, true);
+            
+        }
+        else   
+        {
+            if(nums[i + 1] < nums[i])   // include
+            {
+                 Take = 1 + helper(nums, i + 1, n, true);
+            }
+
+                 notTake = helper(nums, i + 1, n, false);   
+        }
         
-//         if(flag) 
-//         {
-//             if(nums[i + 1] > nums[i])  // include
-//             {
-//                 ans = max(ans, 1 + helper(nums, i + 1, n, false));
-//             }
-//             else  // exclude
-//             {
-//                 ans = max(ans, helper(nums, i + 1, n, true));
-//             }
-//         }
-//         else   
-//         {
-//             if(nums[i + 1] < nums[i])   // include
-//             {
-//                 ans = max(ans, 1 + helper(nums, i + 1, n, true));
-//             }
-//             else  // exclude
-//             {
-//                 ans = max(ans, helper(nums, i + 1, n, false));
-//             }
-//         }
-        
-//         return dp[i][flag] = ans;
-//     }
+        return dp[i][flag] = max(Take,notTake);
+    }
     
-    int wiggleMaxLength(vector<int>& nums) {
-        vector<vector<int>> dp(1002,vector<int>(2,0));
+    int wiggleMaxLength(vector<int>& nums) { 
         int n = nums.size();
         if(n==0)
             return 0;
         int ans = INT_MIN;
-        dp[n-1][0] = 1;
-        dp[n-1][1] = 1;
+        memset(dp, -1, sizeof(dp));
         
-        for(int i=n-2; i>=0; i--) {
-            for(int flag=0; flag<=1; flag++){
-                int ans = 0;
-                if(flag==1) 
-                {
-                    if(nums[i + 1] > nums[i])  // include
-                    {
-                        ans = max(ans, 1 + dp[i + 1][0]);
-                    }
-                    else  // exclude
-                    {
-                        ans = max(ans, dp[i + 1][1]);
-                    }
-                }
-                else   
-                {
-                    if(nums[i + 1] < nums[i])   // include
-                    {
-                        ans = max(ans, 1 + dp[i + 1][1]);
-                    }
-                    else  // exclude
-                    {
-                        ans = max(ans, dp[i + 1][0]);
-                    }
-                }
-                dp[i][flag] = ans;
-            }
-        }
+        ans = max(ans, helper(nums, 0, n, true));
         
+        memset(dp, -1, sizeof(dp));
         
-        return max(dp[0][0],dp[0][1]);
+        ans = max(ans, helper(nums, 0, n, false));
+        
+        return ans;
     }
 };

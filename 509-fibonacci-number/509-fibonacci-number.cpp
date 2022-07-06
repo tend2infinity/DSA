2725367990857155dcbccfebb1cbf808819d0a3e
Soumya Singh <somu12.ss@gmail.com>
2022-07-07 05:15:54 +0530

    Time: 2 ms (64.31%), Space: 6.2 MB (9.60%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int recur(int n, vector<int>& dp){
+        if(n==0)
+            return 0;
+        if(n==1)
+            return 1;
+        if(dp[n]!= -1)
+            return dp[n];
+        return dp[n] =  recur(n-1,dp) + recur(n-2,dp);
+    }
+    int fib(int n) {
+        vector<int>dp(n+1,-1);
+        return recur(n,dp);
+    }
+};

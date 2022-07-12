d3472ce091624c0dfa5b6d724edd97581af9cf5c
Soumya Singh <somu12.ss@gmail.com>
2022-07-12 15:03:02 +0530

    Time: 681 ms (5.04%), Space: 135.5 MB (12.74%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,12 +1,35 @@
 class Solution {
 public:
-int maxResult(vector<int>& nums, int k) {
-	vector<int> dp(size(nums), INT_MIN);
-    multiset<int> s ({ dp[0] = nums[0] });         
-	for(int i = 1; i < size(nums); i++) {
-        if(i > k) s.erase(s.find(dp[i - k - 1]));
-        s.insert(dp[i] = *rbegin(s) + nums[i]);    
+//     int recur( vector<int>& nums, int k, int index, vector<int>& dp){
+//         //base
+//         if(index == nums.size()-1)
+//             return nums[nums.size()-1];
+//         if(index > nums.size()-1)
+//             return -1e8;
+//         if(dp[index] != -1)
+//             return dp[index];
+
+//         int val = nums[index];
+//         int i = 1;
+//         int maxi = -1e8;
+//         int size = nums.size()-1;
+//         while( i<= min(k,size)){
+//             maxi = max(maxi, val+ recur(nums,k,index+i,dp));
+//             i++;
+//         }
+//        return dp[index] =  maxi;
+        
+//     }
+    int maxResult(vector<int>& nums, int k) {
+        vector<int> dp(nums.size()+1, 0);
+        int size = nums.size()-1;
+        dp[0] = nums[0];
+        multiset <int,greater<int>> S = {dp[0]};
+        for(int index = 1; index<=size; index++){             //jumping backwards
+            if(index>k)
+                S.erase(S.find(dp[index-k-1]));
+            S.insert(dp[index] =  nums[index] + *S.begin());
+        }   
+       return dp[size];
     }
-	return dp.back();
-}
 };

d6bcb90864ce6348a16d6cf209e2e21d606c655c
Soumya Singh <somu12.ss@gmail.com>
2022-10-07 02:18:19 +0530

    Time: 844 ms (55.46%), Space: 160.2 MB (86.25%) - LeetHub

---
+++ 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.cpp
@@ -0,0 +1,28 @@
+class LUPrefix {
+public:
+    vector<bool> prefix;
+    int i=0;
+    LUPrefix(int n) {
+        prefix.resize(n+2);
+    }
+    
+    void upload(int d) {
+        // auto x = find(stream.begin(),stream.end(),d);
+        // stream.erase(x);
+        prefix[d] = 1;
+    }
+    
+    int longest() {
+        while(prefix[i+1]==1){
+            i++;
+        }
+        return i;    
+    }
+};
+
+/**
+ * Your LUPrefix object will be instantiated and called as such:
+ * LUPrefix* obj = new LUPrefix(n);
+ * obj->upload(video);
+ * int param_2 = obj->longest();
+ */

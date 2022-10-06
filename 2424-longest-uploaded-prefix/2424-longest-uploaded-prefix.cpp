5cee499e3652ba4f1ab5cc1ab6d1fc7238f72b95
Soumya Singh <somu12.ss@gmail.com>
2022-10-07 02:21:05 +0530

    Time: 903 ms (41.63%), Space: 160.1 MB (86.25%) - LeetHub

--- 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.cpp
+++ 2424-longest-uploaded-prefix/2424-longest-uploaded-prefix.cpp
@@ -3,17 +3,17 @@ public:
     vector<bool> prefix;
     int i=0;
     LUPrefix(int n) {
-        prefix.resize(n+2);
+        prefix.resize(n+1);
     }
     
     void upload(int d) {
         // auto x = find(stream.begin(),stream.end(),d);
         // stream.erase(x);
-        prefix[d] = 1;
+        prefix[d-1] = 1;
     }
     
     int longest() {
-        while(prefix[i+1]==1){
+        while(prefix[i]==1){
             i++;
         }
         return i;    
